370e0bc85617bf14758150de2a575490f187229e
bkocadiz25 <bkocadiz25@ku.edu.tr>
2026-04-02 21:33:44 +0300

    done

--- q1.py
+++ q1.py
@@ -2,4 +2,12 @@ def longest_palindromic_substring(s):
     """
     Given a string find the longest palindromic substring
     """
-    pass
+    longest = ""
+
+    for i in range(len(s)):
+        for j in range(i + 1, len(s) + 1):
+            part = s[i:j]
+            if part == part[::-1] and len(part) > len(longest) and len(part) > 1:
+                longest = part
+
+    return longest
--- q3b.py
+++ q3b.py
@@ -2,4 +2,43 @@ def add_binary(a, b):
     '''
     Given two strings perform binary addition and return the result as a string
     '''
-    pass
+    numA = a.split("b")[1]
+    numB = b.split("b")[1]
+    
+    if len(numA) < len(numB):
+        numA = (len(numB) - len(numA))*"0" + numA
+    elif len(numB) < len(numA):
+        numB = (len(numA) - len(numB))*"0" + numB
+    
+    c = "0"
+    
+    A = numA[::-1]
+    B = numB[::-1]
+    tot = ""
+    
+    for i in range(len(numB)):
+        if A[i] == "0" and B[i] == "0":
+            tot += c
+            c = "0"
+        elif (A[i] == "0" and B[i] == "1") or (B[i] == "0" and A[i] == "1"):
+            if c == "0":
+                tot += "1"
+                c = "0"
+            elif c == "1":
+                tot += "0"
+                c = "1"
+        elif A[i] == "1" and B[i] == "1":
+            if c == "0":
+                tot += "0"
+                c = "1"
+            elif c == "1":
+                tot += "1"
+                c = "1"
+    if c == "1":
+        tot += "1"
+    
+    result = tot[::-1].lstrip("0")
+    if result == "":
+        result = "0"
+            
+    return "0b" + result
